14db1e54ae14fa811f37d85380c158c0c20056f8
Muhammad Shahmir Khan <sshhaahhmmiirr@gmail.com>
2019-08-11 16:19:46 +0500

    Age teller
    1 adding the data

---
+++ age teller 2018/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=0;
+    printf("\n\n\n\n\n\tENTER YOUR BIRTH YEAR %d",a);
+    scanf("%d",&a);
+    printf("\n\n\n\n\t*********    YOUR AGE IS %d  **************** ",2018-a);
+    getch();
+    return 0;
+}
